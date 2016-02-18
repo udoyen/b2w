97874c9c4ef0560557b175d1e991b1a0c1486ca1
udoyen <datameshprojects@gmail.com>
2016-02-18 16:33:18 +0100

    footer done

--- wp-content/themes/bootstrap2wordpress/footer.php
+++ wp-content/themes/bootstrap2wordpress/footer.php
@@ -11,18 +11,26 @@
 
 ?>
 
-	</div><!-- #content -->
-
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'bootstrap2wordpress' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'bootstrap2wordpress' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'bootstrap2wordpress' ), 'bootstrap2wordpress', '<a href="http://georgeudosen.com" rel="designer">george udosen</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
-
 <?php wp_footer(); ?>
+
+<!-- BOOTSTRAP CORE JAVASCRIPT
+				placed at the end of the document so the pages loads faster!
+	 ======================================================= -->
+
+	 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+	 <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/assets/js/jquery-2.2.0.min.js"></script>
+	 <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/assets/js/bootstrap.min.js"></script>
+	 <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/assets/js/main.js"></script>
+
+	 <!-- Typekit Fonts -->
+	 <script src="https://use.typekit.net/qda5lie.js"></script>
+	 <script>
+		 try {
+			 Typekit.load({
+				 async: true
+			 });
+		 } catch (e) {}
+	 </script>
 
 </body>
 </html>
