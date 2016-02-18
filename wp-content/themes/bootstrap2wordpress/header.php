252d9fba83405f4f2cba4c7c39451ed26237c683
udoyen <datameshprojects@gmail.com>
2016-02-18 11:42:00 +0100

    screen shot changed

--- wp-content/themes/bootstrap2wordpress/header.php
+++ wp-content/themes/bootstrap2wordpress/header.php
@@ -52,7 +52,7 @@
               <span class="icon-bar"></span>
             </button>
             <a href="/" class="navbar-brand">
-              <img src="assets/img/logo.png" alt="Bootstrap to Wordpress" /></a>
+              <img src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/logo.png" alt="Bootstrap to Wordpress" /></a>
 
           </div>
           <!-- navbar-header -->
