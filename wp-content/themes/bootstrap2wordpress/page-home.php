92d9cc797e56bf5984fc60b91f35408ad7e5ae1b
udoyen <datameshprojects@gmail.com>
2016-02-19 20:49:31 +0100

    home page video header title correction

--- wp-content/themes/bootstrap2wordpress/page-home.php
+++ wp-content/themes/bootstrap2wordpress/page-home.php
@@ -314,7 +314,7 @@ get_header(); ?>
   <div class="container">
     <div class="row">
       <div class="col-sm-8 col-sm-offset-2">
-        <h2>Watch the Course Intruduction</h2>
+        <h2>Watch the Course Introduction</h2>
         <iframe width="100%" height="415" src="https://www.youtube.com/embed/q-mJJsnOHew" frameborder="0" allowfullscreen></iframe>
       </div>
       <!-- end col -->
