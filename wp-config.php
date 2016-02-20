a56849a76c3cbf4defa9785ccb56b2026bc0b301
udoyen <datameshprojects@gmail.com>
2016-02-20 21:01:08 +0100

    changed permissions to match new wordpress-1 site

--- wp-config.php
+++ wp-config.php
@@ -80,7 +80,7 @@ $table_prefix  = 'gbs_';
  *
  * @link https://codex.wordpress.org/Debugging_in_WordPress
  */
-define('WP_DEBUG', false);
+define('WP_DEBUG', true);
 
 /* That's all, stop editing! Happy blogging. */
 
